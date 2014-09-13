428a2d236e6b0735314b1df51c02a19e6e7eccb2
Peter <peterhsaxton@gmail.com>
2014-09-13 10:51:04 +0100

    dummy index page

---
+++ tasks/build.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var del = require('del');
+
+gulp.task('clean', function () {
+    return del.sync(['www']);
+});
+
+gulp.task('copy', function () {
+    return gulp.src(['app/config.xml', 'app/index.html'], {base: 'app/'})
+        .pipe(gulp.dest('www'));
+});
+
+gulp.task('build', ['clean', 'copy'])
